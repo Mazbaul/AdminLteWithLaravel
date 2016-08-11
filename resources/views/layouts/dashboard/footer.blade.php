a060f9e885784f9ef23c5432a8fa96e1e584eff9
Md Rasel Ahmed <ninja.rasel@gmail.com>
2016-08-12 01:46:43 +0600

    Initialization

---
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Md Rasel Ahmed
+ * Date: 03-Aug-16
+ * Time: 11:00 AM
+ */
+?>
+
+<div class="pull-right hidden-xs">
+    <b>Version</b> 1.0.0
+</div>
+<strong>Copyright &copy; 2016 <a href="#" target="_blank">Company#</a>.</strong> All rights reserved.
