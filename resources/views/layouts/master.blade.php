3853585250b65a4326517d9e92dfa361045662ea
Md Rasel Ahmed <ninja.rasel@gmail.com>
2016-08-12 14:35:16 +0600

    Initialization

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="author" content="Mohammed Abdus Sattar Shohag">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Md Rasel Ahmed">
     <title>
         Amader Rasta - @yield('title')
     </title>
